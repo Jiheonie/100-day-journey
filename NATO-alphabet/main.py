f54daa1064690491eb30d0e6670babbdae454e2a
Jiheonie <hoangtunguyen2002@gmail.com>
2022-08-07 22:16:35 +0700

    create NATO alphabet exercise

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,27 +1,21 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
+from pandas import read_csv
+
+
+data = read_csv('nato_phonetic_alphabet.csv')
+phonetic_dict = {row['letter']:row['code'] for (index, row) in data.iterrows()}
+
+word = input('Enter your word: ').upper()
+letters_list = [phonetic_dict[c] for c in word]
+
+print(letters_list)
+
+
+
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# {"A": "Alfa", "B": "Bravo"}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
---
+++ main.py
@@ -0,0 +1,6 @@
+sentence = 'What is your name?'
+
+
+my_dict = {word:len(word) for word in sentence.split()}
+
+print(my_dict)
