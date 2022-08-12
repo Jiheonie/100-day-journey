0f307d1c7b73f3d8eb79a20eb1eb39d16d5c8522
Jiheonie <hoangtunguyen2002@gmail.com>
2022-08-12 16:14:03 +0700

    add catch error for NATO-alphabet

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,21 +1,19 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 from pandas import read_csv
-
 
 data = read_csv('nato_phonetic_alphabet.csv')
 phonetic_dict = {row['letter']:row['code'] for (index, row) in data.iterrows()}
 
-word = input('Enter your word: ').upper()
-letters_list = [phonetic_dict[c] for c in word]
 
-print(letters_list)
+def generate_phonetic():
+    word = input('Enter your word: ').upper()
+    try:
+        letters_list = [phonetic_dict[c] for c in word]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+        generate_phonetic()
+    else: 
+        print(letters_list)
 
-
-
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+generate_phonetic()
 
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-sentence = 'What is your name?'
+fruits = ['APPLE', 'PEAR', 'ORANGE']
+
+def make_pie(index):
+    try: 
+        fruit = fruits[index]
+    except IndexError:
+        print('Fruit pie')
+    else:
+        print(fruit + ' pie')
+
+make_pie(4)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        total_likes += 0
+        
 
 
-my_dict = {word:len(word) for word in sentence.split()}
 
-print(my_dict)
+print(total_likes)
