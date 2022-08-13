be902ef01d9f8fb88533668c72e9426ffa894499
Jiheonie <hoangtunguyen2002@gmail.com>
2022-08-13 15:14:48 +0700

    update password-manager

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import END, Button, Entry, Label, PhotoImage, Tk, Canvas, messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 
 
@@ -24,9 +25,15 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = website_entry.get()
+    website = website_entry.get().title()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            'Username': username,
+            'Password': password
+        }
+    }
 
     if website == '':
         messagebox.showerror(title='Oops', message="Please enter the website's name you want to save.") 
@@ -35,13 +42,50 @@ def save():
         messagebox.showerror(title='Oops', message="Please enter the password.") 
         return
 
-    is_ok = messagebox.askokcancel(title=website, message=f'There are details entered: \nUsername: {username}\nPassword: {password}\nIs it ok to save?')
+    try:
+        with open('data.json', 'r') as data_file:
+            # reading old data
+            data = json.load(data_file)
+            # updating old data with new data
+            data.update(new_data)
+    except FileNotFoundError:
+        with open('data.json', 'w')  as data_file:
+            json.dump(new_data, data_file, indent=2)
+    except json.decoder.JSONDecodeError:
+        with open('data.json', 'w')  as data_file:
+            json.dump(new_data, data_file, indent=2)
+    else:
+        with open('data.json', 'w')  as data_file:
+            # saving updated data
+            json.dump(data, data_file, indent=2)
+    finally:
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 
-    if is_ok:
-        with open('data.txt', 'a')  as data:
-            data.write(f'{website} | {username} | {password}\n')
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search():
+    website = website_entry.get().title()
+    try:
+        with open('data.json', 'r') as data_file:
+            # reading old data
+            data = json.load(data_file)
+        account = data[website]
+
+    except KeyError as error:
+        messagebox.showerror(title='Oops', message=f'No detail for {error} exists.')
+
+    except FileNotFoundError:
+        messagebox.showerror(title='Oops', message=f'There is no account that has been saved.')
+
+    else:
+        username = account['Username']
+        password = account['Password']
+        messagebox.showinfo(title=website, message=f'Username: {username}\nPassword: {password}\n\n'
+                                                    f'Password has been copied.')
+        pyperclip.copy(password)
+
+        
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,8 +114,12 @@ password_label.grid(row=3, column=0)
 
 # website entry
 website_entry = Entry()
-website_entry.grid(row=1, column=1, columnspan=2, sticky='ew')
+website_entry.grid(row=1, column=1, sticky='ew')
 website_entry.focus()
+
+# search button
+search_button = Button(text='Search', width=14, command=search)
+search_button.grid(row=1, column=2)
 
 # username entry
 username_entry = Entry()
